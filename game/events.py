c86bcad7381ec8f527d8f2e1c27f8d613b60f77e
Nikolaj Lauridsen <nikolajlauridsen@protonmail.ch>
2016-10-20 00:46:36 +0200

    Add D-pad to controls

--- game/events.py
+++ game/events.py
@@ -18,13 +18,17 @@ def check_keydown_event(event, player, settings):
             settings.game_paused = False
         elif not settings.game_paused:
             settings.game_paused = True
-    elif event.key == pygame.K_w and not player.change_y > 0:
+    elif event.key == pygame.K_w and not player.change_y > 0\
+            or event.key == pygame.K_UP and not player.change_y > 0:
         player.go_up()
-    elif event.key == pygame.K_s and not player.change_y < 0:
+    elif event.key == pygame.K_s and not player.change_y < 0\
+            or event.key == pygame.K_DOWN and not player.change_y < 0:
         player.go_down()
-    elif event.key == pygame.K_a and not player.change_x > 0:
+    elif event.key == pygame.K_a and not player.change_x > 0\
+            or event.key == pygame.K_LEFT and not player.change_x > 0:
         player.go_left()
-    elif event.key == pygame.K_d and not player.change_x < 0:
+    elif event.key == pygame.K_d and not player.change_x < 0\
+            or event.key == pygame.K_RIGHT and not player.change_x < 0:
         player.go_right()
 
 
